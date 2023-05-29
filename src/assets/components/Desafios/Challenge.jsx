77feddd608135ba0a984ec8f099e17d5cf1ca168
Al3ncar <alencar29htcss@gmail.com>
2023-05-28 23:58:01 -0300

    Done: Challengegit add .!

--- src/assets/components/Desafios/Challenge.jsx
+++ src/assets/components/Desafios/Challenge.jsx
@@ -1,11 +1,54 @@
 export default function Challenge (){
-    let [n1, n2] = [5, 2] 
+    let [a, b] = [15, 20];
     return( 
         <>
-            <h3>Desafio 1</h3>
-            <p>{n1}</p>
-            <p>{n2}</p>
-            <button onClick={() => console.log(n1 + n2)}> Somar </button>
+            <h3>Desafio 2</h3>
+            <p>A: {a}</p>
+            <p>B: {b}</p>
+            <button onClick={() => console.log(a + b)}> Somar </button>
         </>
     )
+}; 
+
+// ---- Outros codigos com lógicas diferentes ---- //
+
+/* 
+const Challenge = () => {
+    let valor1 = 10, valor2 = 90;
+    function mostra_numero(){
+        return <div>{valor1+valor2}</div>
+    }
+    
+    return (
+        <>
+            <p>{valor1}</p>
+            <p>{valor2}</p>
+
+            <button onClick={mostra_numero}>Clique </button>
+        </>
+    );
+};
+ 
+
+export default Challenge;
+*/
+
+/* import React from 'react'
+
+const Challenge = () => {
+ const a = 10
+ const b = 20
+
+
+  return (
+    <div>
+        <p>A {a}</p>
+        <p>B {b}</p>
+
+        <h1>A soma de a + b é igual a {a + b}</h1>
+    </div>
+  )
 }
+
+export default Challenge
+ */
