d732b0c9221392db1e250496bf6cf4e29867f949
Al3ncar <alencar29htcss@gmail.com>
2023-05-28 21:42:47 -0300

    Return Function

--- src/assets/components/2_FUNDAMENTOS/EventClick.jsx
+++ src/assets/components/2_FUNDAMENTOS/EventClick.jsx
@@ -1,12 +1,36 @@
+
 const EventClick = () => {
-    const handleClick = (e) => {
-        console.log(e)
+    const handleClick = (e) => console.log(e)
+    const handleReturnFalsoOrTrue = (x) => {
+        if(x){
+            return <p>Eu sou true</p>
+        } else {
+            return <p>Eu sou falso</p>
+        }
     }
+
     return(
         <>
             <div>
                 <h3> 4 - EventClick </h3>
                 <button onClick={handleClick}> Click Aqui </button>
+            </div>
+            <div>
+                <h4>1 - funções no evento onClick </h4>
+                <button onClick={() => console.log('hehe fui clicado')}> Click aqui tambem!!</button>
+            </div>
+            <div>
+                <h4>2 - funções no evento onClick </h4>
+                <button onClick={() => {
+                    if(true){
+                        console.log(' Não sei se estou certo ')
+                    }
+                }}> Click aqui tambem!!</button>
+            </div>
+            <div>
+                <h4>2 - funções de renderização</h4>
+                {handleReturnFalsoOrTrue(true)}
+                {handleReturnFalsoOrTrue(false)}
             </div>
         </>
     )
